79e3d59088c1328b6ed6caffa11a08d120837bcd
Timo Dempwolf <timo.dempwolf@pcg.io>
2024-02-07 23:26:30 +0100

    Fix deploy.sh for control_plane

--- cloudrun/control_plane/deploy.sh
+++ cloudrun/control_plane/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook
+nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane
 gcloud run deploy control-plane \
 	--max-instances=2 --no-allow-unauthenticated --region=europe-west1 \
 	--service-account=control-plane-sa@cw-td-sandbox.iam.gserviceaccount.com \
