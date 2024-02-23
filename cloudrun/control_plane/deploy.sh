964523d0acc65ccc3cc301dad642e097c7eb8993
Timo Dempwolf <timo.dempwolf@pcg.io>
2024-02-23 10:27:13 +0100

    Update deployment files

--- cloudrun/control_plane/deploy.sh
+++ cloudrun/control_plane/deploy.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
 nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane
-gcloud run deploy control-plane \
-	--set-env-vars="PROJECT_ID=cw-td-sandbox,ZONE=europe-west1-b" \
-	--set-env-vars="IMAGE_PATH=projects/debian-cloud/global/images/family/debian-12, MACHINE_TYPE=e2-micro" \
-	--max-instances=2 --no-allow-unauthenticated --region=europe-west1 \
-	--service-account=control-plane-sa@cw-td-sandbox.iam.gserviceaccount.com \
+gcloud run deploy ghr-control-plane \
+	--region=europe-west1 \
 	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane
