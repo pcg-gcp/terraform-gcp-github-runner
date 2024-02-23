6311568b133a30116112ffcfea93db7edef79225
Timo Dempwolf <timo.dempwolf@pcg.io>
2024-02-23 22:41:38 +0100

    Update deploy.sh files

--- cloudrun/control_plane/deploy.sh
+++ cloudrun/control_plane/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane
+nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest
 gcloud run deploy ghr-control-plane \
 	--region=europe-west1 \
-	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane
+	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest
--- cloudrun/webhook/deploy.sh
+++ cloudrun/webhook/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook
+nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest
 gcloud run deploy ghr-webhook \
 	--region=europe-west1 \
-	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook
+	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest
