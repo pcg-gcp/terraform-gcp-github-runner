c49f5bcc0cbe5a130e6d8fe4a423cb27a6f20a17
Timo Dempwolf <timo.dempwolf@pcg.io>
2024-03-07 18:25:13 +0100

    Update startup script

--- templates/runners/start_runner.sh
+++ templates/runners/start_runner.sh
@@ -3,17 +3,17 @@
 echo "Starting runner..."
 
 echo "Download runner config"
-config = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/github_runner_config" -H "Metadata-Flavor: Google")
 instance_name = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google")
-runner_user = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/runner_user" -H "Metadata-Flavor: Google")
-runner_dir = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/runner_dir" -H "Metadata-Flavor: Google")
+runner_config = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/attributes/github_runner_config" -H "Metadata-Flavor: Google")
+runner_user = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/attributes/runner_user" -H "Metadata-Flavor: Google")
+runner_dir = $(curl -v "http://metadata.google.internal/computeMetadata/v1/instance/attributes/runner_dir" -H "Metadata-Flavor: Google")
 
 echo "github_runner_config: $github_runner_config"
 
 cd $runner_dir
 
 echo "Configuring runner"
-./config.sh --work _work --name "ghr-$instance_name" $config
+./config.sh --work _work --name "ghr-$instance_name" $runner_config
 
 echo "Installing as service"
 ./svc.sh install $runner_user
