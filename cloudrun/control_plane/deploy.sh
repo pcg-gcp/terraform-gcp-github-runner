f37e3bf45206f5069efa5d6f5bcff294cabf8361
Timo Dempwolf <timo.dempwolf@pcg.io>
2024-11-18 10:27:11 +0100

    chore(deps): Update dependencies

--- cloudrun/control_plane/deploy.sh
+++ cloudrun/control_plane/deploy.sh
@@ -2,5 +2,6 @@
 
 nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest
 gcloud run deploy ghr-control-plane \
-	--region=europe-west1 \
-	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest
+  --region=europe-west1 \
+  --project=cw-td-sandbox \
+  --image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/control-plane:latest
--- cloudrun/webhook/deploy.sh
+++ cloudrun/webhook/deploy.sh
@@ -2,5 +2,6 @@
 
 nerdctl build . -t europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest && nerdctl push europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest
 gcloud run deploy ghr-webhook \
-	--region=europe-west1 \
-	--image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest
+  --region=europe-west1 \
+  --project=cw-td-sandbox \
+  --image=europe-docker.pkg.dev/cw-td-sandbox/docker-repo/webhook:latest
